d47eacab0bd4489ef87864c4ea22412302ac3d0c
kudakovazam@yandex.ru <kudakovazam@yandex.ru>
2024-02-14 21:33:36 +0300

    zxc

--- finder.py
+++ finder.py
@@ -58,7 +58,6 @@ def calculate_height(glass):
     for idx, row in enumerate(glass):
         if 1 in row or 2 in row:
             c = idx
-            print(idx)
             break
     return len(glass) - c
 
@@ -99,17 +98,24 @@ glass = \
      [1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
      [1, 1, 1, 1, 1, 0, 0, 1, 1, 1]]
 
-tetris_figures = [[[0, 1, 0, 0],
-                  [0, 1, 0, 0],
-                  [0, 1, 0, 0],
-                  [0, 1, 0, 0]]]
+tetris_figures = [[[1, 0, 0],
+                  [1, 1, 1,]]]
 # Iterate over all Tetris figures
+figs = {}
 for figure in tetris_figures:
     possible_positions = generate_possible_positions(glass, figure)
     print("Possible positions for figure:")
     for position in possible_positions:
-        print(position)
+        # print(position)
         new_pos = return_final_position(deepcopy(glass), *position)
-        pprint(new_pos)
-        print(calculate_height(new_pos))
-        print(count_empty_cells(new_pos))
+        # pprint(new_pos)
+        tax = calculate_height(new_pos) + count_empty_cells(new_pos)
+        # print(tax)
+        try:
+            figs[tax].append((position, new_pos))
+        except:
+            figs[tax] = []
+            figs[tax].append((position, new_pos))
+best_score = sorted(figs.keys())[0]
+pos = figs[best_score][0][1]
+pprint(pos)
